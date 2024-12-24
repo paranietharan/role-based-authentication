d8245e03009bf3a5193ebea90d44997cd5f4dfd3
paranietharan <142080100+paranietharan@users.noreply.github.com>
2024-12-24 21:28:25 +0530

    Basic Plan

---
+++ user-management/src/pages/AdminEdit.jsx
@@ -0,0 +1,9 @@
+import styles from './styles/AdminEdit.module.css'
+
+function AdminEditPage(){
+    return(
+        <div className={styles.AdminEdit}>AdminEdit</div>
+    )
+}
+
+export default AdminEditPage;
